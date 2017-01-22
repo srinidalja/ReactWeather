ed21e35e953c9e9f3050720a0f078d9fdc4337dc
Srinivas D <srinivas.dalja@icentris.com>
2017-01-22 12:42:37 +0530

    Styled Main page and added example locations in example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,15 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) =>{
     return(
         <div>
-            <h2>Examples</h2>
-            <p>Wellcome to Examples page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are few example locations to try:</p>
+            <ol>
+                <li><Link to="/?locations=Delhi">Delhi</Link></li>
+                <li><Link to="/?locations=Chennai">Chennai</Link></li>
+            </ol>
         </div>
     );
 };
